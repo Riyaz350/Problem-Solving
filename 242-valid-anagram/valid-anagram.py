2c99565b529ca059a4c48a238aee59f6d8f099d9
S.K. Riyaz Ahmed <riyaz3582@gmail.com>
2025-02-11 16:03:19 +0600

    Time: 21 ms (59.94%) | Memory: 14.3 MB (19.54%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,4 +1,21 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        return ''.join(sorted(s)) ==''.join(sorted(t))
-        
+        dic1={}
+        dic2={}
+        if len(s) != len(t):
+            return False;
+        for i in range(len(s)):
+            if s[i] not in dic1:
+                dic1[s[i]] = 1
+            else:
+                dic1[s[i]] += 1;
+            if t[i] not in dic2:
+                dic2[t[i]] = 1
+            else:
+                dic2[t[i]] += 1;
+        for i in s:
+            if i not in dic2:
+                return False;
+            if dic1[i] != dic2[i]:
+                return False;
+        return True
