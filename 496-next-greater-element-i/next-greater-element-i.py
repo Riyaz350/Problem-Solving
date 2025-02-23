0c1a3d8424e85dfc14e8749a5942a81f253e942f
S.K. Riyaz Ahmed <riyaz3582@gmail.com>
2025-02-23 16:21:50 +0600

    Time: 34 ms (31.23%) | Memory: 12.6 MB (53.76%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,12 +1,9 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
-        dic = {}
         ans = []
-        for i, num in enumerate(nums2):
-            if num not in dic: dic[num] = i;
         for i in nums1:
             ind = -1
-            for  j in range(dic[i], len(nums2)):
+            for  j in range(nums2.index(i), len(nums2)):
                 if nums2[j] > i:
                     ind= nums2[j]
                     break
