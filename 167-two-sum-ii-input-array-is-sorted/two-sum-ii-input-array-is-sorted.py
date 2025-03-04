17958f1038446879a555c438342c4384b096bbd2
S.K. Riyaz Ahmed <riyaz3582@gmail.com>
2025-03-04 10:11:09 +0600

    Time: 3 ms (86.24%) | Memory: 12.9 MB (69.92%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,9 @@
 class Solution(object):
-    def twoSum(self,numbers, target):
+    def twoSum(self, numbers, target):
         dic = {}
-
-        for i, num in enumerate(numbers):
+        for i,num in enumerate(numbers):
             if target-num not in dic:
                 dic[num] = i
             else:
                 return [dic[target-num]+1, i+1 ]
+        
