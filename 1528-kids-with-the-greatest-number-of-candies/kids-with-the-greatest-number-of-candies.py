a6c39d400adb495658c2126f406666625e69330d
S.K. Riyaz Ahmed <riyaz3582@gmail.com>
2025-03-15 12:51:50 +0600

    Time: 0 ms (100.00%) | Memory: 12.3 MB (83.13%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         ans = []
-        max_val = sorted(candies)[-1]
+        max_val = max(candies)
         for i in candies:
             if i + extraCandies >= max_val:
                 ans.append(True)
